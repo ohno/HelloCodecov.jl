94dc5c2705ba8fa9fda2a6f0a9470ce3218d05a8
Documenter.jl <documenter@juliadocs.github.io>
2025-08-07 00:01:06 +0000

    build based on 0a2224f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#HelloCodecov","page":"Home","title":"HelloCodecov","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HelloCodecov.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
